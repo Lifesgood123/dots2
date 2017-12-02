cbcedb014323c12ba6a115c2777c9d9835b08afb
Tanner Galyean <sprinklelong@gmail.com>
2017-12-01 23:28:54 -0500

    New scripts

---
+++ auto/for-loop-example.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+print("My Name is")
+total = 0
+for i in range(int(input("Enter a number> ")), int(input("Enter a Higher Number> "))  + 1 ):
+    total = total + i
+print(total)
+
+
--- quad_functions.py
+++ quad_functions.py
@@ -1,5 +1,6 @@
 from math import sqrt as sroot
-from fractions import Fraction 
+from fractions import Fraction
+from random import randint
 
 def f(a, b, c, x):
     y = (a*(x**2)+(b*x)+c)
@@ -43,6 +44,10 @@ print ("x intercepts  = %s, %s \n" % (xint1, xint2))
 print("I/O check?")
 answer = input(">")
 
+if input("Random integers? ") == "y":
+    numbers = [randint(1, 10), randint(11, 100), randint(101, 1000), randint(1001, 10000)]
+    for i in numbers:
+        f(a, b, c, i)
 while answer == "y":
     x = float(input("f(?)"))
     y = f(a, b, c, x)
