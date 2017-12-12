b804a80d95a88fea34d98504019c3a1d405522ab
Tanner Galyean <sprinklelong@gmail.com>
2017-12-12 14:06:49 -0500

    Added documentation

--- car2.py
+++ car2.py
@@ -9,9 +9,15 @@ scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
 client = gspread.authorize(creds)
 
+
+#get Price search criteria from user
 sheet = client.open("Craigslist cars").sheet1
 max_price = "&max_price=" + input("max price? >  ")
 min_price = "&min_price=" + input("min price? >  ")
+##############################################################
+#Contruct list of results
+# Honestly there might be a more efficient way to do this, because 
+# at the moment it holds it in ram and isn't efficient. Might be a better way to do this
 
 def get_list(page):
     my_url = 'https://winstonsalem.craigslist.org/search/cta?s=' + str(page) + max_price + min_price
@@ -21,15 +27,23 @@ def get_list(page):
     page_soup =  bs(page_html, "html.parser")
     result = page_soup.find("ul", {"class":"rows"})
     return result
+
 cars = []
+
 for i in range(0, 9):
     page = 0 
     stuff = get_list(page).findAll("li", {"class":"result-row"})
     for car in stuff:
         cars.append(car)
     page += 120
+
+# The above for loop actually  makes the list.
+###############################################################
+
 print(len(cars))
+
 rowcounter = 2
+# Adding each list item to spreadsheet. 
 for i in cars:
     postlink = i.p.a.get("href")
     postclient = ureq(postlink)
@@ -41,6 +55,9 @@ for i in cars:
     title = i.p.a.text
     holdup = i.p.findAll("span", {"class":"result-price"})
     sheet.update_cell(rowcounter, 1, title)
+
+# Not all items have a price listed, so this for loop checks the length of the holdup list 
+
     if len(holdup) >= 1:
         price = holdup[0].text
     else:
@@ -51,6 +68,9 @@ for i in cars:
     dayoftheyear = i.p.time.get("datetime")
     sheet.update_cell(rowcounter, 4, dayoftheyear)
     rowcounter += 1
+    
+    # Print For debugging
+
     print("\n")
     print(title)
     print(price)
