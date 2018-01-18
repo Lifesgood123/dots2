adf018fbbb73f654be397d4bb396f20d211ba83a
Tanner Galyean <sprinklelong@gmail.com>
2018-01-17 23:26:34 -0500

    Yo

---
+++ bubblesort.py
@@ -0,0 +1,16 @@
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+import random
+stuff = random.sample(range(100), 10)
+pp.pprint(stuff)
+def bubble_sort(seq):
+    changed = True
+    while changed:
+        changed = False
+        for i in range(len(seq) - 1):
+            if seq[i] > seq[i+1]:
+                seq[i], seq[i+1] = seq[i+1], seq[i]
+                changed = True
+        pp.pprint(seq)
+    return None
+bubble_sort(stuff)
