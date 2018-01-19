2b759380f7cf161d9324dc9f56df4d8e746a9b7e
Tanner Galyean <sprinklelong@gmail.com>
2018-01-19 07:02:28 -0500

    IZT WORKS

--- webscraping/full-on-scrapy-goodness.py
+++ webscraping/full-on-scrapy-goodness.py
@@ -19,17 +19,41 @@ def do_stuff(url):
 	stuff = directions.findAll("li",{"class":"step"})
 	for i in ingrid:
 		try:
-			print(i.text)
-			file.write(i.text + '\n')
+			new = i.text.replace('\n', '')
+			new = new + '\n'
+			print(new)
+			file.write(new)
 		except:
 			print("meh")
 	for i in stuff:
 		try:
-			print(i.text)
-			file.write(i.text + '\n')
+			new = i.text.replace('\n', '')
+			new = new + '\n'
+			print(new)
+			file.write(new)
 
 		except:
 			print("meh")
-with open("urlss") as f:
-	for line in f:
-		do_stuff(line)
+def search():
+	url="http://allrecipes.com/search/results/?wt="+ input("search terms> ") +"&sort=re&page=2#2"
+	browser = webdriver.Firefox()
+	browser.get(url)
+	html = browser.page_source
+	soup = bs(html, "html.parser")
+	browser.close()
+	stuff=soup.findAll("article", {"class":"grid-col--fixed-tiles"})
+	head = 'http://allrecipes.com'
+	listt = []
+	links = []
+	for i in stuff:
+		try:
+			listt.append(i.h3.find("a").get('href'))
+		except:
+			continue
+	for i in listt:
+		new = head + i
+		links.append(new)
+
+	for i in links:
+		do_stuff(i)
+search()
