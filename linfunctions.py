77e8b1d919517fa5a9642038192f9636e8d64f20
Tanner Galyean <sprinklelong@gmail.com>
2018-01-29 18:27:08 -0500

    More recipes

--- linfunctions.py
+++ linfunctions.py
@@ -4,31 +4,28 @@ from fractions import Fraction
 from random import randint
 import pylab
 import numpy
-#import argparse
-#parser = argparse.ArgumentParser()
-#parser.add_argument("a", type=int)
-#parser.add_argument("b", type=int)
-#parser.add_argument("c", type=int)
-#parser.add_argument("-g", "--graph", help="display graph of function", action="store_true")
-#parser.add_argument("-v", "--verbose", help="display more information", action='store_true')
 
-#args = parser.parse_args()
 
-m = float(input("slope> "))
-b = float(input("intercept> "))
-x = float(input("x> "))
+def get_slope(p, p1):
+    m = (p[1] - p1[1]) /(p[0] - p1[1])
+    b = get_int(p, m)
+    return m, b
+
+def get_int(p, m):
+    b = m * p[0] - p[1] 
+    return b
 
 def f(x):
-    y = ((m*x)+b)
+    y = (m*x)+b
     print("f("+str(x)+") = "+str(y))
     return y
 
+def graph():
+    y = []
+    x = range(-15, 15)
+    for i in x:
+        y.append(f(i))
 
-y = []
-x = range(-15, 15)
-for i in x:
-    y.append(f(i))
-
-pylab.grid(True)
-pylab.plot(x,y)
-pylab.show()
+    pylab.grid(True)
+    pylab.plot(x,y)
+    pylab.show()
